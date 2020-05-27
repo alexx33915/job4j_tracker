b0b085909b363999a82b6e3a1e4ea86e3cb4135a
alexx33915 <sash-2007@ya.ru>
2020-05-27 19:31:34 +0500

    9. Написать тесты на StartUI.[785#246556]

--- src/main/java/ru/job4j/tracker/showAllAction.java
+++ src/main/java/ru/job4j/tracker/showAllAction.java
@@ -9,9 +9,13 @@ public class showAllAction implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
         Item[] result = tracker.findAll();
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+
+        for(Item i: result){
+            System.out.println(i);
         }
+        //for (int i = 0; i < result.length; i++) {
+         //   System.out.println(result[i]);
+       // }
         return true;
     }
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -46,4 +46,11 @@ public class StartUITest {
         Item item3 = null;
         assertThat(delete, is( item3));
     }
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(  new String[] {"0"}  );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
 }
