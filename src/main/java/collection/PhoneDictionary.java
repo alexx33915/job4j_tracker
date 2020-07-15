2f149037dfe748aeebb796ba7d0679597bc155b4
alexx33915 <sash-2007@ya.ru>
2020-07-15 09:41:32 +0500

    1. Телефонный справочник на базе ArrayList[41598#246514]-1

--- src/main/java/collection/PhoneDictionary.java
+++ src/main/java/collection/PhoneDictionary.java
@@ -1,28 +1,37 @@
 package collection;
 
-
 import java.util.ArrayList;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
 
-    public void add(Person person) {
+    /**
+     * add person in phoneDictionary
+     *
+     * @param person add person in phoneDictionary
+     */
+    public void add(final Person person) {
         this.persons.add(person);
     }
 
     /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * Вернуть список всех пользователей,
+     * который содержат
+     * key в любых полях.
      *
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
-    public ArrayList<Person> find(String key) {
+    public ArrayList<Person> find(final String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key))
-            result.add(person);
+            if (person.getAddress().contains(key)
+                    || person.getName().contains(key)
+                    || person.getPhone().contains(key)
+                    || person.getSurname().contains(key)) {
+                result.add(person);
+            }
         }
-
         return result;
     }
 }
