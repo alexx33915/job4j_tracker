af59ed7bb2785befc8f9bcb9cd69a267d70a8a8a
alexx33915 <sash-2007@ya.ru>
2020-07-31 14:35:57 +0500

    0. Сортировка[10096#246512] -2

--- src/main/java/ru/job4j/tracker/ItemSortByNameAscending.java
+++ src/main/java/ru/job4j/tracker/ItemSortByNameAscending.java
@@ -5,14 +5,6 @@ import java.util.Comparator;
 public class ItemSortByNameAscending implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        if (o1.getName().equals( o2.getName())) {
-            return 0;
-        }
-        if (o1.getName().compareTo(o2.getName())>0) {
-            return 1;
-        }
-        else {
-            return -1;
-        }
+        return o1.getName().compareTo(o2.getName());
     }
 }
--- src/main/java/ru/job4j/tracker/ItemSortByNameDescending.java
+++ src/main/java/ru/job4j/tracker/ItemSortByNameDescending.java
@@ -5,13 +5,6 @@ import java.util.Comparator;
 public class ItemSortByNameDescending implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        if (o1.getName().equals(o2.getName())) {
-            return 0;
-        }
-        if (o2.getName().compareTo(o1.getName()) > 0) {
-            return 1;
-        } else {
-            return -1;
-        }
+        return o2.getName().compareTo(o1.getName());
     }
 }
