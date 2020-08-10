3c746db4e349198446dac6d177b888eeffaa1b7d
alexx33915 <sash-2007@ya.ru>
2020-08-11 01:40:45 +0500

    Компаратор для строк

---
+++ src/main/java/collection/ComparatorForString/StringCompare.java
@@ -0,0 +1,36 @@
+package collection.ComparatorForString;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() > o2.length()) {
+
+            try {
+                for (int i = 0; i < o1.length(); i++) {
+                    int result = Integer.compare(o1.charAt(i), o2.charAt(i));
+                    if (result != 0) {
+                        return result;
+                    }
+                }
+                return 0;
+            } catch (StringIndexOutOfBoundsException e) {
+                return 1;
+            }
+
+        } else {
+            try {
+                for (int i = 0; i < o2.length(); i++) {
+                    int result = Integer.compare(o1.charAt(i), o2.charAt(i));
+                    if (result != 0) {
+                        return result;
+                    }
+                }
+                return 0;
+            } catch (StringIndexOutOfBoundsException e) {
+                return -1;
+            }
+        }
+    }
+}
