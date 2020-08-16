eea3701b8d59ddacb531f5deee22ab78a1e7ae64
alexx33915 <sash-2007@ya.ru>
2020-08-16 20:57:56 +0500

    3. Компаратор для строк [#246508]-3

--- src/main/java/collection/ComparatorForString/StringCompare.java
+++ src/main/java/collection/ComparatorForString/StringCompare.java
@@ -5,31 +5,16 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        if (o1.length() == o2.length()) {
-            for (int i = 0; i < o2.length(); i++) {
-                int result = Integer.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    return result;
-                }
+        int min = Math.min(o1.length(), o2.length());
+
+        for (int i = 0; i < min; i++) {
+            int result = Integer.compare(o1.charAt(i), o2.charAt(i));
+            if (result != 0) {
+                return result;
             }
-            return 0;
         }
-        if (o1.length() > o2.length()) {
-            for (int i = 0; i < o2.length(); i++) {
-                int result = Integer.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    return result;
-                }
-            }
-            return 1;
-        } else {
-            for (int i = 0; i < o1.length(); i++) {
-                int result = Integer.compare(o1.charAt(i), o2.charAt(i));
-                if (result != 0) {
-                    return result;
-                }
-            }
-            return -1;
-        }
+        return Integer.compare(o1.length(), o2.length());
     }
 }
+
+
