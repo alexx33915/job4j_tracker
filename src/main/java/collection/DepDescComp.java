48b7a86e048323b80e3522757f91fee93f10ac1f
alexx33915 <sash-2007@ya.ru>
2020-09-26 11:15:22 +0500

    отсортировать департаменты - коллекции экзаменационное задание-3

--- src/main/java/collection/DepDescComp.java
+++ src/main/java/collection/DepDescComp.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
+
         String[] masStr1 = o1.split("/", 2);
         String[] masStr2 = o2.split("/", 2);
-
 
         int rsl = masStr1[0].compareTo(masStr2[0]);
         if (rsl > 0) {
