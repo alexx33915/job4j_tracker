72d1e80c4ced94d9ae74b2a3513677503ba96b73
alexx33915 <sash-2007@ya.ru>
2020-09-28 11:26:25 +0500

    отсортировать департаменты - коллекции экзаменационное задание-4

--- src/main/java/collection/DepDescComp.java
+++ src/main/java/collection/DepDescComp.java
@@ -8,26 +8,8 @@ public class DepDescComp implements Comparator<String> {
 
         String[] masStr1 = o1.split("/", 2);
         String[] masStr2 = o2.split("/", 2);
-
         int rsl = masStr1[0].compareTo(masStr2[0]);
-        if (rsl > 0) {
-            return -1;
-        } else if (rsl < 0) {
-            return 1;
-        } else {
-
-            if (masStr1.length == 1 || masStr2.length == 1) {
-                return masStr1.length > masStr2.length ? 1 : -1;
-            }
-
-            int rsl2 = masStr1[1].compareTo(masStr2[1]);
-            if (rsl2 > 0) {
-                return 1;
-            } else if (rsl2 < 0) {
-                return -1;
-            }
-        }
-        return 0;
+        return rsl==0 ? o1.compareTo(o2) : (masStr1[0].compareTo(masStr2[0]) >0 ? -1 : 1);
     }
 }
 
