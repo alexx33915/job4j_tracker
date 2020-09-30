24355364c78c1c43288b7a3b006b1145984947b5
alexx33915 <sash-2007@ya.ru>
2020-09-30 15:18:27 +0500

    3. Ссылки на методы.[70608#246380]

--- src/main/java/collection/Departments.java
+++ src/main/java/collection/Departments.java
@@ -1,8 +1,49 @@
 package collection;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Departments {
+
+
+    public static void main(String[] args) {
+        List<String > list = new ArrayList<>();
+        list.add("22");
+        list.add("7777");
+        list.add("10");
+        list.add("5");
+        list.add("455555");
+
+     /*   Comparator<String> eee = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                return o2.length()-o1.length();
+            }
+        };*/
+
+        Consumer<String> consumer  = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+
+              return;
+            }
+        } ;
+
+
+
+
+         //Comparator<String> eee = (left, right)-> right.length()-left.length();
+
+
+
+       // Collections.sort(list, eee);
+
+        System.out.println(list);
+
+
+
+    }
 
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new TreeSet<>() ;
