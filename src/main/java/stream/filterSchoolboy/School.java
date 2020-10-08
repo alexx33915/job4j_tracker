995a4b38317584e92828849c0ddbb02580412a2d
alexx33915 <sash-2007@ya.ru>
2020-10-08 15:10:38 +0500

    1. Фильтрация учеников.[110058#246386]-1

--- src/main/java/stream/filterSchoolboy/School.java
+++ src/main/java/stream/filterSchoolboy/School.java
@@ -3,15 +3,10 @@ package stream.filterSchoolboy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public  class School {
     List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> result = new ArrayList<>();
-        for (Student student : students) {
-            if (predict.test(student)) {
-                result.add(student);
-            }
-        }
-        return result;
+          return  students.stream().filter(predict).collect(Collectors.toList());
     }
 }
