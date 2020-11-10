131496069bc088e263dfb3b55475c9e51d5cab73
alexx33915 <sash-2007@ya.ru>
2020-11-10 12:37:27 +0500

    Экзаменнационное задание  по Stream Api

---
+++ src/main/java/stream/FilterNumbers.java
@@ -0,0 +1,10 @@
+package stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterNumbers {
+    public static List<Integer> filter(List<Integer> data) {
+        return data.stream().filter(x->x%2==0).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/stream/TakeWhileMethodTest.java
@@ -0,0 +1,19 @@
+package stream;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TakeWhileMethodTest {
+    @Test
+    public void test() {
+        assertEquals(
+                List.of(-2, -1),
+                TakeWhileMethod.takeWhile(List.of(3, 2, 1, 0, -2, -1))
+        );
+    }
+}
